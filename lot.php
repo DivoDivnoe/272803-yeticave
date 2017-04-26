f5714f2d548523ea154be0ed0e38df364c3c901f
DivoDivnoe <andrey-tm203@mail.ru>
2017-04-27 01:10:35 +0300

    corrected formatting

--- lot.php
+++ lot.php
@@ -10,46 +10,38 @@ $bets = [
 
 function ts2relative($ts) {
     $twenty_four_hours = 24 * 60 * 60;
-    $one_hour = 60 * 60;
 
     $dif = time() - $ts;
 
     if ($dif > $twenty_four_hours) {
         $date = date('d.m.y в H:i' , $ts);
-    } else if ($dif >= $one_hour) {
-        $date = formatHours(gmdate('H', $dif));
-    } else {
-        $date = formatMinutes(gmdate('i', $dif));
+    }  else {
+        $date = formatTime($dif);
     }
 
     return $date;
 }
 
-function formatMinutes($minutes) {
-    $last_char = substr($minutes, 1, 1);
-    $first_char = substr($minutes, 0, 1);
+function formatTime($ts) {
+    $one_hour = 60 * 60;
 
-    if ($last_char === '1' && $first_char !== '1') {
-        $result_str = $minutes . ' минуту назад';
-    } else if (($last_char === '2' || $last_char === '3' || $last_char === '4') && $first_char !== '1') {
-        $result_str = $minutes . ' минуты назад';
+    if ($ts >= $one_hour) {
+        $timeString = ['часов', 'час', 'часа'];
+        $date = gmdate('H', $ts);
     } else {
-        $result_str = $minutes . ' минут назад';
+        $timeString = ['минут', 'минута', 'минуты'];
+        $date = gmdate('i', $ts);
     }
-
-    return $result_str;
-}
-
-function formatHours($date) {
     $last_char = substr($date, 1, 1);
     $first_char = substr($date, 0, 1);
+    $last_chars = ['1', '2', '3', '4'];
 
-    if ($last_char === '1' && $first_char !== '1') {
-        $result_str = $date . ' час назад';
-    } else if (($last_char === '2' || $last_char === '3' || $last_char === '4') && $first_char !== '1') {
-        $result_str = $date . ' часа назад';
+    if (!in_array($last_char, $last_chars) || $first_char === '1') {
+        $result_str = $date .  ' ' . $timeString[0] . ' назад';
+    } else if ($last_char === '1') {
+        $result_str = $date .  ' ' . $timeString[1] . ' назад';
     } else {
-        $result_str = $date . ' часов назад';
+        $result_str = $date .  ' ' . $timeString[2] . ' назад';
     }
 
     return $result_str;
